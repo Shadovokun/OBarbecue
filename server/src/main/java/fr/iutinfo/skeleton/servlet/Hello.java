d41c155777c378640498a097721e908578fcc72d
Florian PROVO <florian.provo@etudiant.univ-lille1.fr>
2017-03-22 10:16:29 +0100

    Premier commit essai déploiement

--- server/src/main/java/fr/iutinfo/skeleton/servlet/Hello.java
+++ server/src/main/java/fr/iutinfo/skeleton/servlet/Hello.java
@@ -9,13 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "testServlet", urlPatterns = {"/hello"}, initParams = {@WebInitParam(name = "simpleParam", value = "paramValue")})
+@WebServlet(name = "testServlet", urlPatterns = {"/hello"}, initParams = {@WebInitParam(name = "simpleParam", value = "les gens !")})
 public class Hello extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         String simpleParam = getServletConfig().getInitParameter("simpleParam");
-        out.println("Hello World " + simpleParam);
+        out.println("Hello "+ simpleParam);
         out.close();
     }
 
