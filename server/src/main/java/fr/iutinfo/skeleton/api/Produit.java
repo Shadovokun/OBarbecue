ec573bcdc47243e3b1ba3a89ea0efb9bf38bb551
Oscar Baudry <oscar.baudry@etudiant.univ-lille1.fr>
2017-03-27 11:05:37 +0200

    ajout getter setter produits

--- server/src/main/java/fr/iutinfo/skeleton/api/Produit.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/Produit.java
@@ -18,6 +18,14 @@ public class Produit {
 		this.type = type;
 	}
 	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public Produit(){}
 
 	public String getNom() {
